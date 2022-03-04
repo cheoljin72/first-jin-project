ef02b5f1ced01e6b2149bddc2e93b3ee746aaf3e
cheoljin72 <chur7072@naver.com>
2022-03-04 22:37:58 +0900

    third commit

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:prac_instargram/src/components/image_data.dart';
+import 'package:prac_instargram/src/controller/bottom_nav_controller.dart';
 
-class App extends StatefulWidget {
+class App extends GetView <BottomNavController> {
   const App({Key? key}) : super(key: key);
 
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        child: Scaffold(
+        child: Obx(()=> Scaffold(
           appBar: AppBar(),
-          body: Container(),
+          body: IndexedStack(
+            index: controller.pageIndex.value,
+            children: [
+              Container(
+                child: Center(child: Text('HOME')),
+              ),
+              Container(
+                child: Center(child: Text('SEARCH')),
+              ),
+              Container(
+                child: Center(child: Text('UPLOAD')),
+              ),
+              Container(
+                child: Center(child: Text('ACTIVITY')),
+              ),
+              Container(
+                child: Center(child: Text('MYPAGE')),
+              ),
+            ],),
           bottomNavigationBar: BottomNavigationBar(
             type: BottomNavigationBarType.fixed,
             showSelectedLabels: false,
             showUnselectedLabels: false,
-            currentIndex: 0,
+            currentIndex: controller.pageIndex.value,
             elevation: 0,
-            onTap: (value){},
+            onTap: controller.changeBottomNav,
             items: [
               BottomNavigationBarItem(icon: Image.asset(IconsPath.homeOff),
                 activeIcon: ImageData(IconsPath.homeOn),
@@ -51,10 +66,8 @@ class _AppState extends State<App> {
               ),
             ],
           )
-        ),
-        onWillPop: ()async{
-          return false;
-        }
-        );
+        ),),
+        onWillPop: controller.willPopAction,
+    );
   }
 }
